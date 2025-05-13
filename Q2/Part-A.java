c9d8b7d923cc4d525d96cbf8818aae7ed1be332e
nycAndyT66 <andyt66@nycstudents.net>
2025-05-13 11:42:41 -0400

    Update Part-A.java

--- Q2/Part-A.java
+++ Q2/Part-A.java
@@ -1,1 +1,30 @@
+public class SignedText {
+    private String fName;
+    private String lName;
+    
+    public SignedText(String i, String j){
+        fName = i;
+        lName = j;
+    }
 
+    public String getSignature(){
+        if(fName == null){
+            return lName;
+        }
+        return fName.substring(0,1)+"-"+lName;
+    }
+
+    public String addSignature(String txt){
+        if(txt.indexOf(this.getSignature()) == 0){
+            return txt.substring(getSignature().length())+getSignature();
+        }
+
+        else if(txt.indexOf(getSignature()) == txt.length()-getSignature().length()-1){
+            return txt;
+        }
+
+        else{
+            return txt + getSignature();
+        }
+    }
+}
