6294e4e8ae6c61d16f6702166579415fb174fbc5
nycAndyT66 <andyt66@nycstudents.net>
2025-05-15 11:42:18 -0400

    Update 1A.java

--- Q1/1A.java
+++ Q1/1A.java
@@ -1,1 +1,15 @@
-
+public class DogWalker{
+    private int maxDogs;
+    
+    public int walkDogs(int hour){
+        int storeDog = numAvaliableDog(hour);
+        if(maxDogs < storeDog)){
+            update(hour, maxDogs);
+            return maxDogs;
+        }
+        else{
+            update(hour, storeDog);
+            return storeDog;
+        }
+    }
+}
