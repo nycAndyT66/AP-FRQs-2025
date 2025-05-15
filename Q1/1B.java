933853bc3799feaaef29e3dd8d79753497e0073f
nycAndyT66 <andyt66@nycstudents.net>
2025-05-15 11:47:38 -0400

    Update 1B.java

--- Q1/1B.java
+++ Q1/1B.java
@@ -1,1 +1,13 @@
-
+public int dogWalkShift(int startHour, int endHour){
+  int total = 0;
+  for(int i = startHour; i <= endHour; i++){
+      total += walkDogs(i) * 5;
+      if (walkDog(1) == maxDogs){
+        total += 3;
+      }
+      else if(9 <= i && i <=17){
+          total += 3;
+      }
+    }
+  return total;
+}
