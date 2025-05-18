0aa28192ddddd3b57d77ad3f2da88b64a2df5417
nycAndyT66 <andyt66@nycstudents.net>
2025-05-18 01:14:49 -0400

    Update 3A.java

--- Q3/3A.java
+++ Q3/3A.java
@@ -1,1 +1,8 @@
-
+public Round(String[] names){
+    competitorList = new ArrayList<Competitor>();
+    int count = 1;
+    for(String x: names){
+        competitorList.add(new Competitor(x, count));
+        count++;
+    }
+}
