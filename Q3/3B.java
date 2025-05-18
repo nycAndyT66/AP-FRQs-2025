9dc5c0ca8cf6811a8a6a7f00ce8a4f8eb05a94b3
nycAndyT66 <andyt66@nycstudents.net>
2025-05-18 01:23:29 -0400

    Update 3B.java

--- Q3/3B.java
+++ Q3/3B.java
@@ -1,1 +1,15 @@
-
+public ArrayList<Match> buildMatches(){
+    ArrayList<Match> arrMatch = new ArrayList<Match>();
+    if(competitorList.size()%2 == 0){
+        for(int i = 0; i < competitorList.size()/2; i++){
+            arrMatch.add(new Match(competitorList.get(i), competitorList.get(competitorList.size()-1-i)));
+        }
+        return arrMatch;
+    }
+    else{
+        for(int i = 0; i < competitorList.size()/2; i++){
+            arrMatch.add(new Match(competitorList.get(i+1), competitorList.get(competitorList.size()-1-i)));
+        }
+        return arrMatch;
+    }
+}
