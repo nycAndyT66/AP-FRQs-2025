83711f3403c1b33526b012dfe503143c0f83bcc1
nycAndyT66 <andyt66@nycstudents.net>
2025-05-18 01:32:23 -0400

    Update 4B.java

--- Q4/4B.java
+++ Q4/4B.java
@@ -1,1 +1,13 @@
-
+public boolean clearPair(int row, int col){
+    int saveNum = puzzle[row][col];
+    for(int i = row; i < puzzle.length; i++){
+        for(int j = 0; i < puzzle[0].length; j++){
+            if(saveNum == puzzle[i][j] && saveNum+puzzle[i][j] == 10){
+                puzzle[row][col] == 0;
+                puzzle[i][j] == 0;
+                return true;
+            }
+        }
+    }
+    return false;
+}
