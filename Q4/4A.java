bbc2ea48e3f70a1c7ec36a8660ce3f689dfa1d6a
nycAndyT66 <andyt66@nycstudents.net>
2025-05-18 01:27:58 -0400

    Update 4A.java

--- Q4/4A.java
+++ Q4/4A.java
@@ -1,1 +1,9 @@
-
+public SumorSameGame(int numRows, int numCols){
+    puzzle = new puzzle[numRows][numCols];
+    for(int i = 0, i < numRows; i++){
+        for(int j = 0; j < numCols; j++){
+            int random = (int) (Math.random()*9 + 1);
+            puzzle[i][j] = random;
+        }
+    }
+}
